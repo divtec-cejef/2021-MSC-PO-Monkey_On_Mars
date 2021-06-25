eebe674e2e45f8b56e354c51da536718102e6df0
Sévan Bendit <bendit.sevan@gmail.com>
2021-06-25 11:22:06 +0200

    saut fois 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    //initialisation des variables
     public float moveSpeedPlayer;
     public float jumpForce;
+    public int numberJump;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
 
     public bool isJumping;
+    public bool isGrounded;
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
@@ -14,16 +21,23 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeedPlayer * Time.deltaTime * 2;
+        //initialise le "isGrounded" pour savoir quand est-ce que le personne touche le sol
+        isGrounded = Physics2D.OverlapCircle(groundCheckRight.position, groundCheckRadius, collisionLayers) || Physics2D.OverlapCircle(groundCheckLeft.position, groundCheckRadius, collisionLayers);
 
+        //initialise variable pour les mouvement gauche droite
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeedPlayer * Time.deltaTime;
+
+        //test si le boutton de saut est prèssé ou non
         if (Input.GetButtonDown("Jump"))
         {
             isJumping = true;
         }
+
     }
 
     void FixedUpdate()
     {
+        //lance ce qu'il faut pour que le joueur ce déplace de gauche à droite
         MovePlayer(horizontalMovement);
     }
 
@@ -32,10 +46,33 @@ public class PlayerMovement : MonoBehaviour
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-        if(isJumping == true)
+        //si il touche le sol
+        if (isGrounded == true)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false; 
+            //remets la variable du nobre de saut à 0
+            numberJump = 0;
         }
+
+        //si le joueur touche le sol ou le nombr ede saut est plus petit que 3
+        if (isGrounded == true || numberJump < 3)
+        {
+            //alors si le boutton de saut est prèssé
+            if (isJumping == true)
+            {
+                //fait un saut
+                jumpForce = 250;
+                rb.AddForce(new Vector2(0f, jumpForce));
+                isJumping = false;
+                numberJump++;
+            }
+        }
+    }
+
+    //sert à gère si les gizmos touche une zone de colision ou non
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckLeft.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(groundCheckRight.position, groundCheckRadius);
     }
 }
