bdb127a5999f802a06a706b3f160556acadf62d1
Sévan Bendit <bendit.sevan@gmail.com>
2021-06-25 09:42:05 +0200

    Faire monter la plateforme avec le singe sans retomber

--- Assets/LeapMotion/Core/Scripts/MovePlateforme.cs
+++ Assets/LeapMotion/Core/Scripts/MovePlateforme.cs
@@ -8,6 +8,9 @@ public class MovePlateforme : MonoBehaviour
 {
     Rigidbody2D m_Rigidbody;
     float m_Speed;
+    public GameObject monkey;
+    float posPlateforme;
+
 
     //position de la main du leap motion
     public Transform transformPalm;
@@ -21,16 +24,39 @@ public class MovePlateforme : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody2D>();
         //Set the speed of the GameObject
         m_Speed = 10.0f;
+        
+
     }
 
     void Update()
     {
-       
-        print(transformPalm.position.y);
+
+
+
+        //pour que la plateforme suive le singe avec un espace de 2
+        //transformMe.position = new Vector3(transformPalm.position.x * (-100), monkey.transform.position.y - 2, transformMe.position.z);
+
+        
+
+        posPlateforme = monkey.transform.position.y;
+
+
+        print(posPlateforme);
+
+        if (monkey.transform.position.y <= (transformMe.position.y + 2))
+        {
+            //la plateforme ne doit pas descendre
+            transformMe.position = new Vector3(transformPalm.position.x * (-100), transformMe.position.y, transformMe.position.z);
+        }
+        else
+        {
+            transformMe.position = new Vector3(transformPalm.position.x * (-100), monkey.transform.position.y - 2, transformMe.position.z);
+        }
+
+        
 
         //Nous faisons des calculs pour positionner la plateforme correctement devant l'écran
-        transformMe.position = new Vector3(transformPalm.position.x * (-100), transformPalm.position.y * 40 + 65, transformMe.position.z);
-        
+        //transformMe.position = new Vector3(transformPalm.position.x * (-100), transformPalm.position.y * 40 + 65, transformMe.position.z);
         
     }
 }  
