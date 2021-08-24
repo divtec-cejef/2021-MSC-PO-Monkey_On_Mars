bd78c3cadb030e92ec85521ec55e272484e606c3
Sévan Bendit <bendit.sevan@gmail.com>
2021-08-24 08:25:45 +0200

    Ajustement téléportation gauche droite

--- Assets/Scripts/MonkeyTeleportSide.cs
+++ Assets/Scripts/MonkeyTeleportSide.cs
@@ -12,23 +12,17 @@ public class MonkeyTeleportSide : MonoBehaviour
 
     void Update()
     {
-        //test si la variable pointsJoueur et plus petit que la position du singe
-        if (pointsJoueur < (int)monkey.transform.position.y + 3)
-        {
-            //donne les points au joueur par rapport à sa position en hauteur
-            pointsJoueur = (int)monkey.transform.position.y + 3;
-        }
-        //si le singe va à moins que -9 dans l'axe X
-        if (monkey.transform.position.x < -9)
+        //si le singe va à moins que -11.30 dans l'axe X
+        if (monkey.transform.position.x < (float)-11.30)
         {
             //il se téléporte à droite
-            monkey.transform.position = new Vector3(9, monkey.transform.position.y, 0);
+            monkey.transform.position = new Vector3((float)11.26, monkey.transform.position.y, 0);
         }
-        //si le singe va à plus que 9 dans l'axe X
-        if (monkey.transform.position.x > 9)
+        //si le singe va à plus que 11.26 dans l'axe X
+        if (monkey.transform.position.x > (float)11.26)
         {
             //il se téléporte à gauche
-            monkey.transform.position = new Vector3(-9, monkey.transform.position.y, 0);
+            monkey.transform.position = new Vector3((float)-11.30, monkey.transform.position.y, 0);
         }
     }
 }
