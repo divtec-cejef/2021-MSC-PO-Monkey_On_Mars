3c6fdfb244dee33cace00af24ad9596af507c7be
Sévan Bendit <bendit.sevan@gmail.com>
2021-09-22 08:39:25 +0200

    Ajout du NoFriction aux platefromes

--- Assets/Scripts/PlateformeBoxCollider.cs
+++ Assets/Scripts/PlateformeBoxCollider.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class PlateformeBoxCollider : MonoBehaviour
 {
-
     public GameObject monkey;
     private bool collider;
-
-
-
-
+    public Material noFriction;
 
     void Update()
     {
-        
-
         if (collider == false)
         {
-
-
             if (monkey.transform.position.y >= gameObject.transform.position.y + (float)0.65)
             {
 
-                BoxCollider2D boxCollider2D = gameObject.AddComponent<BoxCollider2D>();
+                GetComponent<BoxCollider2D>().enabled = true;
+
                 collider = true;
             }
         }
